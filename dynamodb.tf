de21d8ff5dcd5cb3b0825cca93a756b01d3e2b0f
FoxKids <gantsevich10@gmail.com>
2025-06-12 11:59:28 +0300

    Terraform init/plan

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,4 +1,12 @@
 resource "aws_dynamodb_table" "my_dyndb" {
   name         = var.dynamodb_table_name
   hash_key     = var.dynamodb_hash_key
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+
+  attribute {
+    name = var.dynamodb_hash_key
+    type = "S"
+  }
 }
