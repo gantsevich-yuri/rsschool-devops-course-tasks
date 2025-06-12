34acbabc771ed0f0001ad4dcbd364393ce4066e6
FoxKids <gantsevich10@gmail.com>
2025-06-12 23:16:20 +0300

    fmt check

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "my_dyndb" {
-  name         = var.dynamodb_table_name
-  hash_key     = var.dynamodb_hash_key
+  name           = var.dynamodb_table_name
+  hash_key       = var.dynamodb_hash_key
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
