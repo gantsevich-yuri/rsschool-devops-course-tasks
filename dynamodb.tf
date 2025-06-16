60409cf30881885e280959e822d0908332c91cc5
FoxKids <gantsevich10@gmail.com>
2025-06-16 23:55:54 +0300

    fmt check

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "my_dyndb" {
-  name           = var.dynamodb_table_name
-  hash_key       = var.dynamodb_hash_key
-  billing_mode   = "PAY_PER_REQUEST"
+  name         = var.dynamodb_table_name
+  hash_key     = var.dynamodb_hash_key
+  billing_mode = "PAY_PER_REQUEST"
 
   attribute {
     name = var.dynamodb_hash_key
